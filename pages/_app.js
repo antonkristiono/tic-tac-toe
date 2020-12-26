20ae55a9a275d299f1fe06da06e9ea6320e11ddb
Anton Kristiono <antonkristiono47@gmail.com>
2020-12-26 19:12:16 +0700

    auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import { Provider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+      <Provider
+    options= {{
+      clientMaxAge:0,
+      keepAlive:0,
+    }}
+    session={pageProps.session}>
+      <Component {...pageProps} />
+      </Provider>
+  )
 }
 
 export default MyApp
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,10 +6,18 @@ const options = {
     providers : [
         Providers.Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.GOOGLE_CLIENT_SECRET,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            // authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
         }),
     // Can add provider again, please insert on here
     ],
-
-
+    callbacks: {
+        // signIn: async (user, account, profile) => { return Promise.resolve(true)  },
+        // redirect: async (url, baseUrl) => { return Promise.resolve(baseUrl)  },
+        // session: async (session, user) => { return Promise.resolve(session)  },
+        // jwt: async (token, user, account, profile, isNewUser) => { return Promise.resolve(token)  }
+    },
+    debug:true,
 }
+
+export default (req,res) => NextAuth(req, res, options)
